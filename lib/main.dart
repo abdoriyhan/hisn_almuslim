5b590253357f90e90c9ebeb9e090fdcabf711280
abdoriyhan <abdulrahman.riyhan@gmail.com>
2026-06-23 08:00:44 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.yellow,
         ),
         primaryColor: Colors.blue,
-        //useMaterial3: true,
+        useMaterial3: true,
       ),
 
       /*
